2c767946f483d9350e6a355dbf3dab5c1f5d67f4
Alexander Gaidarenko <94627591+Dyadyasashka@users.noreply.github.com>
2023-04-15 14:28:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 # Установка зависимостей
+sudo apt-get install -y libgl1-mesa-glx
+pip install  libgl1-mesa-glx
 pip install -r requirements.txt
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:$LD_LIBRARY_PATH
 
