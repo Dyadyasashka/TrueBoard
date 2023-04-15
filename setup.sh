490f741ae6e1bda4a018350adc1b3747214f33d2
Alexander Gaidarenko <94627591+Dyadyasashka@users.noreply.github.com>
2023-04-15 14:23:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Установка зависимостей
 pip install -r requirements.txt
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:$LD_LIBRARY_PATH
 
 # Запуск приложения
 streamlit run true_board_app.py
